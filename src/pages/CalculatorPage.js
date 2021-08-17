dae178011139472cc9c2289df6336dd6cacfc09b
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-17 14:51:12 -0300

    Apply styles to the pages so they could look similar to the wireframe

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Calculator from '../components/Calculator';
 
 const CalculatorPage = () => (
-  <>
+  <div className="calculator-container">
     <h2>Let&apos;s do some math!</h2>
     <Calculator />
-  </>
+  </div>
 );
 
 export default CalculatorPage;
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Quote = () => (
   <div className="quote-container">
-    <h4>
+    <h3>
       Mathematics is not about numbers, equations, computations, or
       algorithms: it is about understanding. - William Paul Thurston
-    </h4>
+    </h3>
   </div>
 );
 
