83514c5cd4fe2ee11f09cbc76bd622020fae25df
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-17 14:06:56 -0300

    Add inner content to the CalculatorPage, Home and Quote pages

---
+++ src/pages/CalculatorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => {
+  return (
+    <>
+      <h1>Let's do some math!</h1>
+      <Calculator />
+    </>
+  )
+}
+
+export default CalculatorPage; 
---
+++ src/pages/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Quote = () => {
+  return (
+    <div className="quote-container">
+      <h2>
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding. - William Paul Thurston
+      </h2>
+    </div>
+  )
+}
+
+export default Quote;
