8a11eb910c491c3c369a4c4754a05fa11a7f44db
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-18 13:36:53 -0300

    Fix Eslint errors

--- src/logic/tests/operate.test.js
+++ src/logic/tests/operate.test.js
@@ -27,22 +27,22 @@ describe('test the operate module', () => {
   }
 
   test('adds two numbers', () => {
-    expect(Number(operate(2, 3, '-'))).toEqual(-1)
+    expect(Number(operate(2, 3, '-'))).toEqual(-1);
   });
 
   test('adds two numbers', () => {
-    expect(Number(operate(2, 3, 'x'))).toEqual(6)
+    expect(Number(operate(2, 3, 'x'))).toEqual(6);
   });
 
   test('adds two numbers', () => {
-    expect(Number(operate(2, 3, '÷'))).toBeCloseTo(0.666666)
+    expect(Number(operate(2, 3, '÷'))).toBeCloseTo(0.666666);
   });
 
   test('adds two numbers', () => {
-    expect(operate(2, 0, '÷')).toEqual("Can't divide by 0.")
+    expect(operate(2, 0, '÷')).toEqual("Can't divide by 0.");
   });
 
   test('adds two numbers', () => {
-    expect(Number(operate(2, 3, '%'))).toEqual(2)
+    expect(Number(operate(2, 3, '%'))).toEqual(2);
   });
 });
